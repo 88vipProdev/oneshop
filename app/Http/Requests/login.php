39e2442bd96316e6445e1ec04c9213e6f57a3313
88vipProdev <nguyentunglam18091998abc@gmail.com>
2024-06-13 14:20:05 +0700

    login requests

---
+++ app/Http/Requests/login.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class login extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "email"=>"required|email",
+            "password"=>"required"
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            "email.required"=> "không được để trống",
+            "email.exists"=> "email chưa được đăng ký",
+            "password.required"=> "không được để trống",
+            "password.exists"=>"mật khẩu không tông tại",
+        ];
+    }
+
+    public function validateCredentials(){
+        $users = Auth::User();
+
+        if (!$users || !Hash::check($this->input('password'), $users->password)) {
+            return false;
+        }
+        return true;
+    }
+}
